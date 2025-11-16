fd10e04f2f58b2290ec44395b0edaa1fa61cd745
enguy-hub <enguy-hub@github.com>
2025-11-16 21:53:16 +0100

    feat: open external links in new tabs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
   site: 'https://enguy-hub.github.io',
   output: 'static',
   integrations: [react(), mdx()],
   markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+          rel: ['noopener', 'noreferrer']
+        }
+      ]
+    ],
     shikiConfig: {
       themes: {
         light: 'github-light',
